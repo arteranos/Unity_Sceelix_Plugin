9967d1231e1ecc80c481a02db4ee9900fab21577
willneedit <willneedit@nurfuerspam.de>
2023-07-07 01:22:05 +0200

    Updated and fixed the terrain processor

--- Processors/Components/TerrainProcessor.cs
+++ Processors/Components/TerrainProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Assets.Sceelix.Contexts;
 using Assets.Sceelix.Utils;
 using Newtonsoft.Json.Linq;
@@ -17,14 +18,21 @@ namespace Assets.Sceelix.Processors.Components
             if (gameObject.GetComponent<Terrain>() != null)
                 return;
 
-            var heights = jtoken["Heights"].ToObject<float[,]>();
+            var heightsResolution = jtoken["HeightsResolution"].ToVector2();
+            var heightsBytes = jtoken["Heights"].ToObject<byte[]>();
+            var heights = heightsBytes.ToTArray<float>((int) heightsResolution.x, (int) heightsResolution.y);
             var resolution = jtoken["Resolution"].ToObject<int>();
             var sizes = jtoken["Size"].ToVector3();
 
             // AltspaceVR specific: Copy the default material to avoid Altspace using a cut down version
             Material terrainMat = context.CreateOrGetAssetOrResource("Sceelix Terrain Default.mat", () =>
             {
-                Shader shad = Shader.Find("Nature/Terrain/Standard");
+                Shader shad = null;
+
+                shad = Shader.Find("Universal Render Pipeline/Terrain/Lit");
+                if(shad == null)
+                    shad = Shader.Find("Nature/Terrain/Standard");
+
                 Material mat = new Material(shad);
                 return mat;
             });
@@ -46,21 +54,31 @@ namespace Assets.Sceelix.Processors.Components
                     var defaultTexture = Texture2D.whiteTexture.ToMipmappedTexture();
                     List<TerrainLayer> terrainLayers = new List<TerrainLayer>();
 
-                    var tileSize = materialToken["TileSize"].ToVector2();
-                    foreach (JToken textureToken in materialToken["Textures"].Children())
+                    int layerNum = 0;
+                    foreach(JToken layerToken in materialToken["Layers"].Children())
                     {
-                        var name = textureToken["Name"].ToObject<String>();
+                        var tileSize = layerToken["TileSize"].ToVector2();
 
-                        TerrainLayer layer = context.CreateOrGetAssetOrResource("Layer_" + name + ".asset",
+                        var textureToken = layerToken["Texture"];
+
+                        // var name = textureToken["Name"].ToObject<String>();
+                        TerrainLayer layer = context.CreateOrGetAssetOrResource($"Layer_{layerNum}.asset",
                             () => new TerrainLayer()
                             {
-                                diffuseTexture = String.IsNullOrEmpty(name) 
-                                    ? defaultTexture
-                                    : context.CreateOrGetAssetOrResource(name + ".asset", () => textureToken["Content"].ToTexture()),
+                                diffuseTexture = Texture2DExtensions.CreateOrGetTexture(context, layerToken["DiffuseMap"]) ?? defaultTexture,
+                                normalMapTexture = Texture2DExtensions.CreateOrGetTexture(context, layerToken["NormalMap"], true),
                                 tileSize = tileSize
                             });
 
                         terrainLayers.Add(layer);
+                        layerNum++;
+
+                        //terrainLayers.Add(new TerrainLayer()
+                        //{
+                        //    diffuseTexture = Texture2DExtensions.CreateOrGetTexture(context, layerToken["DiffuseMap"]) ?? defaultTexture,
+                        //    normalMapTexture = Texture2DExtensions.CreateOrGetTexture(context, layerToken["NormalMap"], true),
+                        //    tileSize = new Vector2(terrainData.size.x / tileSize.x, terrainData.size.z / tileSize.y)
+                        //});
                     }
 
                     terrainData.terrainLayers = terrainLayers.ToArray();
@@ -70,13 +88,14 @@ namespace Assets.Sceelix.Processors.Components
                 return terrainData;
             });
 
-            
+            var materialToken = jtoken["Material"];
 
-            // UNITY BUG: Doesn't allow terrain assets to be initialized in one go, we need to
-            // save and reload the asset first, then we can add the splat map and save it.
-            var splatmap = jtoken["Material"]["Splatmap"].ToObject<float[,,]>();
-            newTerrain.SetAlphamaps(0, 0, splatmap);
+            var splatMapSize = materialToken["SplatmapSize"].ToVector3();
+            var splatMapBytes = materialToken["Splatmap"].ToObject<byte[]>();
+            float[,,] splatMap = splatMapBytes.ToTArray<float>((int) splatMapSize.x, (int) splatMapSize.y, (int) splatMapSize.z);
+            newTerrain.SetAlphamaps(0, 0, splatMap);
             context.RefreshAssets();
+
 
             //finally, create the terrain components
             Terrain terrain = gameObject.AddComponent<Terrain>();
