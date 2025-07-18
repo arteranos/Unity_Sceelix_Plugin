e6dcde27e19583abcbcc2d9943b1a4b5cff28e24
willneedit <willneedit@nurfuerspam.de>
2025-07-18 09:07:42 +0200

    Added use of UInt32 IndexFormat for huge meshes

--- Common/Processors/Components/MeshFilterProcessor.cs
+++ Common/Processors/Components/MeshFilterProcessor.cs
@@ -17,6 +17,9 @@ namespace Assets.Sceelix.Processors.Components
             {
                 var mesh = new Mesh();
 
+                if (SystemInfo.supports32bitsIndexBuffer)
+                    mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+
                 mesh.vertices = meshToken["Positions"].Children().Select(x => JTokenExtensions.ToVector3(x)).ToArray();
                 mesh.normals = meshToken["Normals"].Children().Select(x => x.ToVector3()).ToArray();
                 mesh.colors = meshToken["Colors"].Children().Select(x => x.ToColor()).ToArray();
@@ -28,20 +31,27 @@ namespace Assets.Sceelix.Processors.Components
 
                 mesh.subMeshCount = triangleSetList.Count;
 
+                bool warned = false;
                 for (int index = 0; index < triangleSetList.Count; index++)
                 {
                     int[] subList = triangleSetList[index].Children().Select(x => x.ToObject<int>()).ToArray();
 
                     int highest = subList.Max();
-                    if (highest > UInt16.MaxValue)
+                    if (highest > UInt16.MaxValue && !warned)
                     {
-                        if (SystemInfo.supports32bitsIndexBuffer)
-                            Debug.LogError($"{meshName}: More than 65535 vertices, which is not supported in this platform. Split up your meshes into smaller parts.");
+                        // If one submesh was too large, the following submeshes will have too high indices, too.
+                        if (!SystemInfo.supports32bitsIndexBuffer)
+                            Debug.LogError($"{meshName}: Highest mesh index is greater than 65534, which is not supported in this platform. Split up your meshes into smaller parts.");
                         else
-                            Debug.LogWarning($"{meshName}: More than 65535 vertices, which may be unsupported on other platform. Suggest to split up your meshes.");
+                            Debug.LogWarning($"{meshName}: Highest mesh index is greater than 65534, which may be unsupported on other platforms. Suggest to split up your meshes.");
+
+                        warned = true;
                     }
+
                     mesh.SetTriangles(subList, index);
                 }
+
+                mesh.Optimize();
 
                 return mesh;
             });
