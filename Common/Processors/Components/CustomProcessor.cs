5f8fd03c937ca7df4294e78b7b89c5fe7c65afdd
willneedit <willneedit@nurfuerspam.de>
2025-09-21 11:27:27 +0200

    Fix: Custom Unity components will be  looked into a 'Sceelix.User'  assembly

--- Common/Processors/Components/CustomProcessor.cs
+++ Common/Processors/Components/CustomProcessor.cs
@@ -12,13 +12,31 @@ namespace Assets.Sceelix.Processors.Components
     {
         public override void Process(IGenerationContext context, GameObject gameObject, JToken jtoken)
         {
+            // Look for the 'default' assembly holding the Unity scripts
+            Assembly ass = null;
+            foreach (Assembly a in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                // Debug.Log($"Assembly: {a.GetName().Name}");
+                if (a.GetName().Name == "Sceelix.User")
+                {
+                    ass = a;
+                    break;
+                }
+            }
+
+            if (ass == null)
+            {
+                Debug.LogWarning("There is no 'Sceelix.User' assembly. Create an assembly definition file named 'Sceelix.User' and place the scripts in its directory.");
+                return;
+            }
+
             var componentName = jtoken["ComponentName"].ToObject<String>();
 
             //if the optimized processor does not exist, simply go for the slower, but very generic Reflection approach
-            var componentType = GetType().Assembly.GetType(componentName);
+            var componentType = ass.GetType(componentName);
 
             if (componentType == null)
-                Debug.LogWarning(String.Format("Component '{0}' is not defined.", componentName));
+                Debug.LogWarning($"Component '{componentName}' is not defined in the 'Sceelix.User' assembly.");
             else
             {
                 Component customComponent = gameObject.AddComponent(componentType);
